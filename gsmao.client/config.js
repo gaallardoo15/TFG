d69ac3fb8b6a95cc064fcbe9ab4c34e536beb187
Javier Gallardo <jgallardo@suitelec.com>
2025-05-20 19:08:09 +0200

    Eliminadas imágenes que no me interesan. Metido icono logo y ultimado detalle final login

--- gsmao.client/config.js
+++ gsmao.client/config.js
@@ -1,31 +1,9 @@
 export const CLIENT = client;
 
 export const CONFIG = {
-    // hitachi: {
-    //     tituloPrincipal: "GSMAO HITACHI",
-    //     logo: "/LogoHitachi_Reduc.png",
-    //     imagenLogin: "",
-    //     logicaDelNegocio: {
-    //         RolesObligadosAAdjuntarFotoAlCerrarOrden: [
-    //             "OPERARIO",
-    //             "RESPONSABLE",
-    //             "RESPONSABLE_TALLER",
-    //             "RESPONSABLE_MATERIALES",
-    //             "JEFE_MANTENIMIENTO",
-    //         ],
-    //         RolesObligadosAAdjuntarFotoMaterialAlCerrarOrden: [
-    //             "OPERARIO",
-    //             "RESPONSABLE",
-    //             "RESPONSABLE_TALLER",
-    //             "RESPONSABLE_MATERIALES",
-    //             "JEFE_MANTENIMIENTO",
-    //         ],
-    //         UsuariosPermitidosModificarFechasOrdenCerrada: ["acastro@hitachi.com"],
-    //     },
-    // },
     gmao: {
         tituloPrincipal: "GMAO",
-        logo: "/LogoSuitelec_Reduc.ico",
+        logo: "/logoGMAO.png",
         imagenLogin: "",
         logicaDelNegocio: {
             RolesObligadosAAdjuntarFotoAlCerrarOrden: ["OPERARIO"],
